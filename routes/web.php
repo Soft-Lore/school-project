34aa536431d365f74d8b4bbd5b3481f86344c375
Moises6669 <moisesalex132@gmail.com>
2025-05-01 20:43:09 -0600

    remove: unnecessary files

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return json_encode([
+        'message' => 'Welcome to the Laravel API!'
+    ]);
 });
